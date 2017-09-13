9983b0424295309d85865fea2d490d3a50bf345f
kullias <akovalenko@xbsoftware.com>
2017-09-13 13:33:05 +0300

    [add] step 2, toolbar and basic style

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var toolbar = {
-	css:"draft", template:"<span class='title'>Toolbar</span>", height: 40
+	css:"topbar", padding:1, view:"toolbar", height:40, cols:[
+		{ view:"button", type:"icon", icon:"arrow-left", width: 40 },
+		{ view:"button", type:"icon", icon:"dashboard", label:"Boards", width:100 },
+		{ view:"search", width:300 },
+		{ view:"label", label:"<a href='https://webix.com/kanban/'>Webix Boards</a>"},
+		{ view:"button", type:"icon", icon:"plus", width: 40 },
+		{ view:"button", type:"icon", icon:"question-circle-o", width: 40 },
+		{ view:"button", type:"icon", icon:"bell-o", width: 40 }
+	]
 };
 var subbar = {
 	css:"draft", template:"<span class='title'>Subbar</span>", height: 40
@@ -9,15 +17,15 @@ var boards = {
 };
 
 var menu = {
-	css:"draft", template:"<span class='title'>Menu</span>", width: 340
+	css:"draft menuarea", template:"<span class='title'>Menu</span>", width: 340
 };
 
 webix.ready(function(){
 	webix.ui({
-		rows:[
+		type:"clean", rows:[
 		  toolbar,
-		  { cols:[
-			  { rows:[ subbar, boards]},
+		  { type:"clean", cols:[
+			  { type:"clean", rows:[ subbar, boards]},
 			  menu
 		  ]}
 		]
